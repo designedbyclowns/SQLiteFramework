980a43e05d5066de8fabb7dd7fdcfe857dceade2
Joe Heck <heckj@mac.com>
2024-02-15 11:26:09 -0800

    WORKING

--- build.sh
+++ build.sh
@@ -13,12 +13,9 @@ cd $SRC_DIR
 
 export CC=clang
 
-for SDK_PLATFORM_NAME in macosx # iphoneos iphonesimulator
+for SDK_PLATFORM_NAME in macosx iphoneos iphonesimulator
 do
 	echo $SDK_PLATFORM_NAME
-
-	make clean
- 	make distclean
 
  	INSTALL_DIR=${BUILD_DIR}/${SDK_PLATFORM_NAME}
  	SDKROOT="$(xcrun --sdk $SDK_PLATFORM_NAME --show-sdk-path)"
@@ -26,21 +23,31 @@ do
  	case $SDK_PLATFORM_NAME in
 
  		macosx)
- 			export CFLAGS="-arch arm64 -arch x86_64"
-			export HOST="aarch64-apple-darwin"
+			HOST_FLAGS="-arch arm64 -arch x86_64 -isysroot ${SDKROOT}"
+ 			export CFLAGS="${HOST_FLAGS}"
+ 			export CXXFLAGS="${HOST_FLAGS}"
+			export LDFLAGS="${HOST_FLAGS}"
+			export CHOST="aarch64-apple-darwin"			
  			;;
  		iphoneos)
- 			export CFLAGS="-arch arm64"
- 			export HOST="aarch64-apple-ios"
+			HOST_FLAGS="-arch arm64 -isysroot ${SDKROOT} -DSQLITE_NOHAVE_SYSTEM"
+ 			export CFLAGS="${HOST_FLAGS}"
+ 			export CXXFLAGS="${HOST_FLAGS}"
+			export LDFLAGS="${HOST_FLAGS}"
+ 			export CHOST="aarch64-apple-ios"
  			;;
 		iphonesimulator)
-			export CFLAGS="-arch x86_64 -arch arm64"
-			export HOST="aarch64-apple-iossimulator"
+			HOST_FLAGS="-arch arm64 -arch x86_64 -isysroot ${SDKROOT} -DSQLITE_NOHAVE_SYSTEM"
+ 			export CFLAGS="${HOST_FLAGS}"
+ 			export CXXFLAGS="${HOST_FLAGS}"
+			export LDFLAGS="${HOST_FLAGS}"
+			export CHOST="aarch64-apple-iossimulator"
 			;;
   	esac
 
- 	sh ${SRC_DIR}/configure --build=aarch64-apple-darwin --host=${HOST} --prefix=${INSTALL_DIR}
+ 	sh ${SRC_DIR}/configure --host=${CHOST} --prefix=${INSTALL_DIR} --enable-static --disable-shared
 
+	make clean
  	make
  	make install
 
@@ -59,8 +66,8 @@ rm -f ${PROJECT_DIR}/fullsqlite3.xcframework.zip
 
 xcodebuild -create-xcframework \
     -library ${BUILD_DIR}/macosx/lib/libsqlite3.a -headers ${BUILD_DIR}/macosx/include \
+   	-library ${BUILD_DIR}/iphoneos/lib/libsqlite3.a -headers ${BUILD_DIR}/iphoneos/include \
     -output ${BUILD_DIR}/fullsqlite3.xcframework
-#    -library ${BUILD_DIR}/iphoneos/lib/libsqlite3.a -headers ${BUILD_DIR}/iphoneos/include \
 #    -library ${BUILD_DIR}/iphonesimulator/lib/libsqlite3.a -headers ${BUILD_DIR}/iphonesimulator/include \
 
 

900cf3577e1dc1b76839ddb94a5dfa6fa305b515
Jeff Johnston <hishma@users.noreply.github.com>
2024-02-18 17:13:40 -0800

    Sign the framework - #1

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -x
 PROJECT_ROOT=${PWD}
 BUILD_DIR=${PROJECT_ROOT}/build
 SRC_DIR=${PROJECT_ROOT}/src/latest
+FRAMEWORK_NAME=fullsqlite3.xcframework
 
 echo $BUILD_DIR
 echo $SRC_DIR
@@ -64,20 +65,23 @@ make distclean
 
 cd $PROJECT_ROOT
 
-echo "▸ Create xcframework"
+echo "→ Create xcframework"
 
-rm -rf ${BUILD_DIR}/fullsqlite3.xcframework
-rm -f ${PROJECT_DIR}/fullsqlite3.xcframework.zip
+rm -rf ${BUILD_DIR}/${FRAMEWORK_NAME}
+rm -f ${PROJECT_DIR}/${FRAMEWORK_NAME}.zip
 
 xcodebuild -create-xcframework \
     -library ${BUILD_DIR}/macosx/lib/libsqlite3.a -headers ${BUILD_DIR}/macosx/include \
    	-library ${BUILD_DIR}/iphoneos/lib/libsqlite3.a -headers ${BUILD_DIR}/iphoneos/include \
     -library ${BUILD_DIR}/iphonesimulator/lib/libsqlite3.a -headers ${BUILD_DIR}/iphonesimulator/include \
-    -output ${BUILD_DIR}/fullsqlite3.xcframework
+    -output ${BUILD_DIR}/${FRAMEWORK_NAME}
+    
+echo "→ code-sign the XCFramework"   
+codesign --timestamp -v --sign "Apple Development: Jeff Johnston (7277MSQ5PT)" "${BUILD_DIR}/${FRAMEWORK_NAME}"
 
-echo "▸ Compress xcframework"
-ditto -c -k --sequesterRsrc --keepParent "${BUILD_DIR}/fullsqlite3.xcframework" "${PROJECT_ROOT}/fullsqlite3.xcframework.zip"
+echo "→ Compress xcframework"
+ditto -c -k --sequesterRsrc --keepParent "${BUILD_DIR}/${FRAMEWORK_NAME}" "${PROJECT_ROOT}/${FRAMEWORK_NAME}.zip"
 
-echo "▸ Compute checksum"
-openssl dgst -sha256 "${PROJECT_ROOT}/fullsqlite3.xcframework.zip" | cut -d ' ' -f2 > checksum.txt 
+echo "→ Compute checksum"
+openssl dgst -sha256 "${PROJECT_ROOT}/${FRAMEWORK_NAME}.zip" | cut -d ' ' -f2 > checksum.txt 
 
